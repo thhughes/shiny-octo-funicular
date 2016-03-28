60b05a22c2d985719f87f865124df60c668c8b27
Troy Hughes <Troy.h.hughes@gmail.com>
2016-03-28 18:10:04 -0400

    Mostly done, need to fix Multiple definition and add more tests

--- Homework2/homework2.rkt
+++ Homework2/homework2.rkt
@@ -77,6 +77,7 @@
              [if0S (c t e) (if0C (desugar c)(desugar t)(desugar e))]
              [appS (f arg) (appC (desugar f)(map desugar arg))]
              [funS (params body) (funC params (desugar body))]
+             ;; desugar:withS: This is creating a function call to a function being defined now
              [withS (bindings body) (appC (funC (map defS-name bindings) (desugar body))
                                           (map desugar (map defS-val bindings)))]
              )
@@ -103,7 +104,7 @@
 
 
 ;---------------------------------------------------------------------------------
-;; interp -- returning a default/dummy value so file can be run
+;; interp -- returning a value
 (define (interp [e : ExprC] [env : Env]) : Value
   (type-case ExprC e
              [numC (n) (numV n)]
@@ -112,25 +113,19 @@
              [multC (l r) (numCMath '* (interp l env) (interp r env))]
              [if0C (c t e) (cond [(is-if0C-zero (interp c env))(interp t env)]
                                  [else (interp e env)])]
-    ;; appC takes an f and arg: exprC and listof ExprC
-    ;; We interp f to get the closure of what to do:
-    ;; Then we have to evaluate that:
-    ;; -- Interp the body with the environment being formed from the
-    ;; -- Closure Environment + Bindings from: (closure(args)) bound to: (appC(args))
+             ;; interp.appC: evaluates a function. Must lookup the closure for the function (f)
+             ;; ;; And interp it with the extended environment gotten from synthesizing the
+             ;; ;; Closure environment + bindings from closure(args) bound to the appC(args). 
              [appC (f arg)(let ([closure (interp f env)])
                             (interp (closV-body (valid-closV closure))
                                     (append (new-env-for-interp (closV-arg (valid-closV closure))
                                                                 arg
                                                                 env)
                                             (closV-env (valid-closV closure)))))]
+             ;; interp:funC: creates closures for the envirionment -- This is when a function is declared
              [funC (param body)(closV param body env)]
              )
   )
-;;(interp (closV-body f-val)
-;;                                    (cons (bind (closV-arg f-val)
-;;                                                (interp arg env))
-;;                                          (closV-env f-val)))
-;; (3 4 5) will choke on closV-body
 
 ;; Interp helpers:
 ;; valid-closV :: Value -> listof symbols or error
@@ -142,20 +137,25 @@
     ))
 
 
-;; new-env-for-interp :: listof symbol,passed Env, listof exprC -> listof binding
-;; Takes in: closV args, environment, appC args
+;; new-env-for-interp :: listof symbol, Env, listof exprC -> listof binding
+;; Takes in a list of symbols (closure arguments), list of ExprC's (arg's in an appC),
+;;   an env (the env called in interp).
+;; This matches up the closure arguments with the list of exprC's passed and binds them
+;;   To create the 'extended environment' to append to the interp environment
 (define (new-env-for-interp [c-arg : (listof symbol)][a-arg : (listof ExprC)][env : Env]) : Env
-  (cond [(= (length c-arg)(length a-arg))(error 'appC "incorrect number of args")]
+  (cond [(not (equal? (length c-arg)(length a-arg)))(error 'appC "incorrect number of args")]
         [else (map2 (lambda (c a) (bind c (interp a env))) c-arg a-arg)]
         )
   )
 
 
 
-;; lookup symbols in environment:
+
+;; Looks up a symbol in a given environment and returns the value of the symbol from the binding in the environment
+;; symbol, env -> Value
 (define (lookup [n : symbol][env : Env]) : Value
   (cond
-    [(empty? env) (error 'lookup "Symbol was not found in the envirionment")]
+    [(empty? env) (error 'lookup "unbound error: symbol not found in environment")]
     [else (let* ([envF (first env)]
                  [name (bind-name envF)]
                  [body (bind-val envF)])
@@ -165,19 +165,28 @@
     ))
 
 
-;; Math function
+;; numCMath is the math function:
+;; -- Function takes in a symbol and 2 values:
+;; --- If Symbol is: '+' and l and r are NumV's, will return a numV of the sum of the two
+;; --- If Symbol is: '*' and l and r are NumV's, will return a numV of the product of the two
+;; --- If l and r are NumV's , and symbol is something other than '+' or '*' raise: invalid operator
+;; -- If at least one of l and r is not a NumV raise 'numCMath error of 'type error: 
+;; symbol, Value, Value -> Value
 (define (numCMath [s : symbol][l : Value][r : Value]) : Value
   (cond [(and (numV? l) (numV? r))
          (cond [(symbol=? s '+)(numV (+ (numV-n l)(numV-n r)))]
                [(symbol=? s '*)(numV (* (numV-n l)(numV-n r)))]
                [else (error 'numCMath "Invalid Operator")])]
-         [else (error 'numCMath "One argument is not a number")]
+         [else (error 'numCMath "type error: left and right should evaluate to numV")]
         ))
 
-;; If Case:
+
+;; If Evaluation: Returns True if condition is a NumV and 0
+;; If condition is not a NumV: raise "type error"
+;; value -> boolean
 (define (is-if0C-zero [condition : Value]) : boolean
-  (cond [(numV? condition)(= 0 (numV-n condition))]
-        [else (error 'if0C "Type Error: Condition should evaluate to NumV")])
+  (cond [(numV? condition)(equal? 0 (numV-n condition))]
+        [else (error 'if0C "type error: condition should evaluate to NumV")])
   )
 
    
@@ -199,47 +208,42 @@
 ;--------------------------------------------------------------------------------
 ;; Tests
 
-;; Tests;; 14 Total
+; Tests;; 14 Total
+"Testing simple math and embedded functions"
 (test (run '(+ 3 4)) (numV 7))
 (test (run '(- 4 2)) (numV 2))
 (test (run '(* 2 4)) (numV 8))
 (test (run '(+ (- (* 1 2) (* 1 1)) 1)) (numV 2))
 
+"Testing if statements"
 (test (run '(if0 (* 1 0) 1 2)) (numV 1))
-;;(test (run '(if0 "Hello WOrld" 1 2)) "type")                    ;; Some Error 
-;; Test single parameter things
-(test (run '(with ([f (fun (x) (* x x))] (f 3)))) (numV 9))     ;; Result
-;; Test multiple parameter things
-(test (run '(with ([f (fun (x y) (* y x))](f 3 5)))) (numV 15)) ;; Result
-(test (run '((fun (x) 5))) (numV 5))                                   ;; I belive this should be legal 
-;; Function Calling a function:
-(test (run '(with ([f (fun (x y) (*x y))][g (fun (z) (* z z))]) (f 1 (g 2))) ) (numV 4))
+
+"Test single parameter functions"
+(test (run '(with ([f (fun (x) (* x x))]) (f 3))) (numV 9))
+
+"Test multiple parameter functions"
+(test (run '(with ([f (fun (x y) (* y x))])(f 3 5))) (numV 15))
+
+"Testing functions being declared and run simultaneously"
+(test (run '((fun (x) 5) 5)) (numV 5))                                   
+
+"Testing multiple functions being called in functions"
+(test (run '(with ([f (fun (x y) (* x y))][g (fun (z) (* z z))]) (f 1 (g 2)))) (numV 4))
+
+"Testing multiple declerations in with"
+(test (run '(with ([x 1][y 2]) (+ x y))) (numV 3))
+
+"Testing Static Scoping"
+(test (run '(with ([x 1]) (with ([f (fun (y) (+ y x))]) (with ([x 2]) (f 10))))) (numV 11))
 
 
-
+"Testing Errors"
+"- Unbound by Undeclared Variable"
 (test/exn (run '(with ((x 3)(y 4)) (+ x (* z y)))) "unbound")
-(test/exn (run '(+ 2 (with ((x 3)(x 4)) (+ x (* x y))))) "multiple")
+
+"- Multiple by multiple definition"
+(test/exn (run '(+ 2 (with ((x 3)(x 4)) (+ x (* x 1))))) "multiple")
+
+"- Type by improper passing to function"
 (test/exn (run '(+ (fun (x) x) 5)) "type")
-(test/exn (run '(if0 (+ 1 0) 2)) "expected");; Tests;; 14 Total
-(test (run '(+ 3 4)) (numV 7))
-(test (run '(- 4 2)) (numV 2))
-(test (run '(* 2 4)) (numV 8))
-(test (run '(+ (- (* 1 2) (* 1 1)) 1)) (numV 2))
-
-(test (run '(if0 (* 1 0) 1 2)) (numV 1))
-(test (run '(if0 "Hello WOrld" 1 2)) "type")                    ;; Some Error 
-;; Test single parameter things
-(test (run '(with ([f (fun (x) (* x x))] (f 3)))) (numV 9))     ;; Result
-;; Test multiple parameter things
-(test (run '(with ([f (fun (x y) (* y x))](f 3 5)))) (numV 15)) ;; Result
-(test (run '((fun (x) 5))) 5)                                   ;; I belive this should be legal 
-;; Function Calling a function:
-(test (run '(with ([f (fun (x y) (*x y))][g (fun (z) (* z z))]) (f 1 (g 2))) ) (numV 4))
-
-
-
-(test/exn (run '(with ((x 3)(y 4)) (+ x (* z y)))) "unbound")
-(test/exn (run '(+ 2 (with ((x 3)(x 4)) (+ x (* x y))))) "multiple")
-(test/exn (run '(+ (fun (x) x) 5)) "type")
-(test/exn (run '(if0 (+ 1 0) 2)) "expected")
 
